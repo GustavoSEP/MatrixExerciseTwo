b23a158ba67e983b711bd475cfb453c00adeb6e6
Gustavo Moreira <gustamoreira26@gmail.com>
2023-04-18 21:10:46 -0300

    Fazer um programa para ler dois números inteiros (M, N), e depois ler uma matriz de M linhas por N colunas contendo números inteiros.

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -1,30 +1,55 @@
-﻿int n = int.Parse(Console.ReadLine());
+﻿using System;
 
-int[,] mat = new int[n, n];
-
-for (int i = 0; i < n; i++)
+namespace Course
 {
-    string[] values = Console.ReadLine().Split(' ');
-    for (int j = 0; j < n; j++)
+    class Program
     {
-        mat[i, j] = int.Parse(values[j]);
+        static void Main(string[] args)
+        {
+
+            string[] line = Console.ReadLine().Split(' ');
+            int m = int.Parse(line[0]);
+            int n = int.Parse(line[1]);
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-
-Console.WriteLine("Main diagonal: ");
-for (int i = 0; i < n; i++)
-{
-    Console.Write(mat[i, i] + " ");
-}
-Console.WriteLine();
-
-int count = 0;
-
-for (int i = 0;i < n; i++)
-{
-    for (int j = 0;j < n; j++)
-    {
-        if (mat[i,j] < 0) { count++; }
-    }
-}
-Console.WriteLine("Negativa Numbers: " + count);
